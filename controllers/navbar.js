eaa9947cc6ca7c7b13af7947781f809421749766
rickparrish <github@rick.parrish>
2015-01-30 18:55:49 -0500

    Make active class work with navbar

---
+++ controllers/navbar.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+var navbar = angular.module('navbar', []);
+
+navbar.controller('navbar', '$scope', function ($scope) {
+    $scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+});
