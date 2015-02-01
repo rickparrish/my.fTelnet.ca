bd6f367146bd115f3cf2e16d637ec5ff899728dc
rickparrish <github@rick.parrish>
2015-01-31 19:16:44 -0500

    Ensure existing users don't get fucked by the Settings.ProxyServer change

--- controllers/addressbook.js
+++ controllers/addressbook.js
@@ -20,6 +20,13 @@ AddressBook.controller('AddressBook', ['$scope', '$filter', '$http', function ($
     };
     if (localStorage['Settings']) {
         $scope.Settings = JSON.parse(localStorage['Settings']);
+
+        // Old ProxyServer setting was only a piece of the domain, is 'us-ga:1123:11235' instead of 'proxy-us-ga.ftelnet.ca:1123:11235'
+        if ($scope.Settings.ProxyServer.indexOf('.') == -1) {
+            var HostPorts = $scope.Settings.ProxyServer.split(':');
+            $scope.Settings.ProxyServer = 'proxy-' + HostPorts[0] + '.ftelnet.ca:' + HostPorts[1] + ':' + HostPorts[2];
+            localStorage['Settings'] = JSON.stringify($scope.Settings);
+        }
     }
     
     // Load the "Add new entry" form and clear it
--- controllers/settings.js
+++ controllers/settings.js
@@ -8,6 +8,13 @@ Settings.controller('Settings', ['$scope', '$http', function ($scope, $http) {
     };
     if (localStorage['Settings']) {
         $scope.Settings = JSON.parse(localStorage['Settings']);
+
+        // Old ProxyServer setting was only a piece of the domain, is 'us-ga:1123:11235' instead of 'proxy-us-ga.ftelnet.ca:1123:11235'
+        if ($scope.Settings.ProxyServer.indexOf('.') == -1) {
+            var HostPorts = $scope.Settings.ProxyServer.split(':');
+            $scope.Settings.ProxyServer = 'proxy-' + HostPorts[0] + '.ftelnet.ca:' + HostPorts[1] + ':' + HostPorts[2];
+            localStorage['Settings'] = JSON.stringify($scope.Settings);
+        }
     }
 
     $scope.ProxyServers = [];
