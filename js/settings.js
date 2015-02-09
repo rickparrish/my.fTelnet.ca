ed3e76fc7defa5f4615717e66763f8a644f28f39
rickparrish <github@rick.parrish>
2015-02-08 20:37:56 -0500

    Update for new json structure

--- js/settings.js
+++ js/settings.js
@@ -11,13 +11,13 @@ $('#cboProxyServer').change(function () {
 function LoadProxyServers() {
     $.getJSON("//proxy.ftelnet.ca/proxyservers.json", function(data) {
         data.sort(function (a, b) {
-            var aName = a.Country.toLowerCase() + a.Location.toLowerCase();
-            var bName = b.Country.toLowerCase() + b.Location.toLowerCase(); 
+            var aName = a.Country.toLowerCase() + a.City.toLowerCase();
+            var bName = b.Country.toLowerCase() + b.City.toLowerCase(); 
             return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0)); 
         });
 
         for (var i = 0; i < data.length; i++) {
-            $('#cboProxyServer').append('<option value="' + data[i].Hostname + ':' + data[i].WsPort + ':' + data[i].WssPort + '">' + data[i].Country + ' (' + data[i].Location + ')</option>');
+            $('#cboProxyServer').append('<option value="' + data[i].Hostname + ':' + data[i].WsPort + ':' + data[i].WssPort + '">' + data[i].Country + ' (' + data[i].City + ')</option>');
         }
         
         var Settings = GetSettings();
